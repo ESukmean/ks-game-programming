2ddc0bed653dafcdbd87142d6f14a1d593502891
ESukmean <esukmean@esukmean.com>
2021-09-26 23:47:20 +0900

    입력에 프로그램이 얽메이지 않도록 코드 구현 1) 입력을 getchar로 하되, non-blocking 모드로 수정 2) frame 단위 스크린 지정을 하고, 스크린을 넘어갈 때에도 키 입력이 유실되지 않도록 별도의 입력버퍼 생성 3) 효율을 위해 입력버퍼는 ring buffer로 함 4) 프레임 제한 장치를 만듦

--- week-5/hw.cpp
+++ week-5/hw.cpp
@@ -1,74 +1,185 @@
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
+#include <algorithm>
+#include <stdio.h>
+#include <termios.h>
+
+class frame;
+class Game;
+class ringbuffer;
 
 const int NORMAL = 0;
 const int NOOP = 10;
 const int TERMINATE = -10;
 
 class frame {
+private:
+	unsigned char* framebuffer; // utf8 고려 한 글자당 3byte 고정
 public:
-	void process();
-
-	static frame* opening();
+	int process(Game*);
+	void render(Game*);
 };
 class ringbuffer {
 	private:
-		char* inner;
-		char* cur_read;
-		char* cur_write;
-		char* pos_end;
+		int* inner;
+		int* cur_read;
+		int* cur_write;
+		int* pos_end;
 
-		char* put_buf;
+		int* put_buf;
 
 		bool is_full = false;
 		int capacity;
 	public:
 		ringbuffer(int capacity) {
 			this->capacity = capacity;
-			this->inner = new char[capacity];
-			this->put_buf = new char[capacity];
+			this->inner = new int[capacity];
+			this->put_buf = new int[capacity];
 			this->pos_end = this->inner + capacity;
 			this->cur_write = this->cur_read = this->inner;
+
+			// 인풋에 영향을 받으면 안되니까 stdin을 넌블럭킹으로 설정
+			struct termios oldSettings, newSettings;
+			tcgetattr( fileno( stdin ), &oldSettings );
+			newSettings = oldSettings;
+			newSettings.c_lflag &= (~ICANON & ~ECHO);
+			tcsetattr( fileno( stdin ), TCSANOW, &newSettings );    
+
+			fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL) | O_NONBLOCK);
 		}
 		~ringbuffer() {
 			delete[] this->inner;
+			delete[] this->put_buf;
 		}
-	
-		int remain() {
+
+		void stdin_read() {
+			int result = 0;
+			int avilable = this->buffer_avilable();
+			int* put_buf = this->put_buf;
+			int read_len = 0;
+			for (; read_len < avilable; read_len++) {
+				if((result = getchar()) == -1) break;
+				// printf("--> %d\n", result);
+				*put_buf = result;
+
+				put_buf++;
+			}
+			
+
+			this->put_enque(read_len);		
+		}
+		int buffer_avilable() {
 			if (is_full) return 0;
 
 			if (this->cur_read < this->cur_write) return this->capacity - (this->cur_write - this->cur_read);
-			// return this->capacity - (this->capacity + this->cur_write) - this->cur_read;
+			if (this->cur_read == this->cur_write) return this->capacity;
 			return this->cur_read - this->cur_write;
 		}
+		int buffer_length() {
+			return this->capacity - this->buffer_avilable();
+		}
+		// char* put_arr() {
+		// 	// 0으로 초기화(정리)후 던져줌.
+		// 	memset(this->put_buf, NULL, this->capacity);
+		// 	return this->put_buf;
+		// }
+		void put_enque(int read_len) {
+			if (read_len == 0) return;
+
+			// printf("read %s", this->put_buf);
+			// printf("read_len %d\n", read_len);
+
+			// if (read_len > this->buffer_avilable()) { 버퍼 수용량 이상으로 들어오면 마지막은 그냥 drop 하도록 함. 아래에서 바운더리 체크 필요 }
+			if (this->cur_read < this->cur_write) {
+				int tail_length = (this->pos_end - this->cur_write);
+				int tail_to_write = std::min(tail_length, read_len * (int)sizeof(int));
+				memcpy(this->cur_write, this->put_buf, tail_to_write);
+
+				if (tail_to_write < read_len) {
+					// 쓸 게 더 남았다면...
+					int head_to_write = std::min((read_len - tail_to_write) * (int)sizeof(int), (int)(this->cur_read - this->inner));
+					memcpy(this->inner, this->put_buf + tail_to_write, head_to_write);
+
+					this->cur_write = this->inner + head_to_write / sizeof(int);
+				} else {
+					// 뒤쪽에 쓴거로 끝이면
+					this->cur_write += tail_to_write / sizeof(int);
+					if (this->cur_write == this->pos_end) this->cur_write = this->inner;
+				}
+			} else {
+				int avilable = this->buffer_avilable();
+				int length = std::min(read_len, avilable > 0 ? avilable : this->capacity);
+				memcpy(this->cur_write, this->put_buf, length * sizeof(int));
+
+				this->cur_write += length;
+				// printf("cur advanced %d\n", this->cur_write - this->inner); 
+			}
+
+			if(this->cur_read == this->cur_write) {
+				this->is_full = true;
+			}
+		}
+
+		void print_content() {	
+			int length = this->buffer_length();
+			// printf("content length %d // ", length);
+			if (length == 0) return;
+
+			int* cur = this->cur_read;
+			for (int i = 0; i < length; i++) {
+				// printf("%#04x() ", *cur);
+				cur++;
+
+				if (cur == this->pos_end + 1) cur = this->inner;
+			}
+			// printf("\n");
+		}
 };
+
+class screen_opening : public frame {
+private:
+	
+public:
+	int process(Game* state) {
+
+	}
+	void render(Game* state) {
+
+	}
+};
+
 class Game {
 private:
 	int lastloop_ts = 0; // input과 process에 의해서 시간 지연이 생길 수 있음. 루프 실행 timestamp를 이용해서 sleep 기간을 보정함.
-	char* inputs; // 비동기로 처리할 것이기도 하고, frame의 전환에서도 입력을 유실하지 않게 따로 input 버퍼를 가지게 함. 이걸로 입력 처리를 해야함
 	int inputs_len = 0;
+	ringbuffer inputs = ringbuffer(1024); // 비동기로 처리할 것이기도 하고, frame의 전환에서도 입력을 유실하지 않게 따로 input 버퍼를 가지게 함. 이걸로 입력 처리를 해야함
 	frame* current_screen; // 화면 처리의 최대단위. DP의 전략패턴을 이용할 것임. frame 아래에서 직접 다음 screen을 수정할 수도 있음을 주의
 
 	int state = NORMAL; // 현재 게임의 전체상태
 
 public:
 	Game() {
-		this->current_screen = frame::opening();
-		this->inputs = new char[1024];
-
-		// 인풋에 영향을 받으면 안되니까 stdin을 넌블럭킹으로 설정
-		fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL) | O_NONBLOCK);
+		this->current_screen = new screen_opening();
 	}
 	~Game() {
-		delete[] this->inputs;
+		delete this->current_screen;
 	}
 	void input() {
-		read(STDIN_FILENO, this->inputs, sizeof(this->inputs));
+		this->inputs.stdin_read();
+		
 	}
-	int process();
-	void render();
+	int process(){
+		return this->current_screen->process(this);
+	}
+	void render(){
+		this->current_screen->render(this);
+	}
 
-	void frame_limit(int fps);
+	void frame_limit(int fps){
+		int wait_us = 1000000 / fps; // (1 / fps * 1000(us) * 1000(ms))
+		usleep(wait_us);
+	}
 
 	bool is_run() {
 		// if (this->state == TERMINATE) return false;
@@ -77,6 +188,7 @@ public:
 		return this->state != TERMINATE;
 	}
 };
+
 
 
 int main() {
@@ -88,6 +200,6 @@ int main() {
 		gameInstance.process();
 		gameInstance.render();
 
-		gameInstance.frame_limit(60);
+		gameInstance.frame_limit(2);
 	}
 }
