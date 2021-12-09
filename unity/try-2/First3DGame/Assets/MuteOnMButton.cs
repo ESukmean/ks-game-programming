b200541ab32f6de861cb3d15d85638ea88250cce
이석민(2017648070, ESukmean) <esukmean@esukmean.com>
2021-12-09 10:32:31 +0900

    시간의 흐름까지 나오게 추가·

---
+++ unity/try-2/First3DGame/Assets/MuteOnMButton.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteOnMButton : MonoBehaviour
+{
+    public AudioSource audio;
+
+    private bool is_pause = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("m"))
+        {
+            if (is_pause)
+            {
+                audio.Play(0);
+            } else
+            {
+                audio.Pause();
+            }
+        }
+
+        is_pause = !is_pause;
+    }
+}
---
+++ unity/try-2/First3DGame/Assets/TimeTick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeTick : MonoBehaviour
+{
+    public Text clock;
+    private double StartTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartTime = Time.realtimeSinceStartup;
+        clock = GameObject.Find("TimeElpased").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int time = (int)(Time.realtimeSinceStartup - StartTime);
+
+        clock.text = (time / 60) + "분 " + (time % 60) + "초";
+    }
+}
